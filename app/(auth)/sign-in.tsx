325833e2e35a60cf222ea84cb74fe83f3d0007a4
Raj Kapuriya <rajkapuriya03@gmail.com>
2026-04-14 09:58:27 +0530

    feat:project setup,nativewind,routing and tab navigartion

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'expo-router';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>SignIn</Text>
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Create Account
+      </Link>
+
+      <Link href="/" className="mt-4 rounded bg-primary text-white p-4">
+        Create Account
+      </Link>
+    </View>
+  );
+};
+
+export default SignIn;
